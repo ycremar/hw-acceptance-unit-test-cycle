a4fcee21c253b408fe180c9bb3acc67cbb7b04d8
ycremar <ec2-user@ip-172-31-32-70.us-east-2.compute.internal>
2019-10-22 18:42:42 +0000

    add director column

--- rottenpotatoes/app/models/movie.rb
+++ rottenpotatoes/app/models/movie.rb
@@ -2,4 +2,9 @@ class Movie < ActiveRecord::Base
   def self.all_ratings
     %w(G PG PG-13 NC-17 R)
   end
+  
+  def self.movies_by_director(movie)
+    return nil if movie.director.blank?
+    Movie.all.where(director: movie.director)
+  end
 end
