16397e31d2d8c70a5d6586f0b3b928d1c6203d79
ycremar <ec2-user@ip-172-31-32-70.us-east-2.compute.internal>
2019-10-22 21:23:45 +0000

    search by director

--- rottenpotatoes/config/routes.rb
+++ rottenpotatoes/config/routes.rb
@@ -2,5 +2,5 @@ Rottenpotatoes::Application.routes.draw do
   resources :movies
   # map '/' to be a redirect to '/movies'
   root :to => redirect('/movies')
-  match '/movies/:id/movies_by_director', to: 'movies#movies_by_director', as: 'movies_by_director', via: :get
+  get '/movies/:id/movies_by_director' => 'movies#movies_by_director', :as => :movies_by_director
 end
